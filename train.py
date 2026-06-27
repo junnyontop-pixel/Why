b573f89ceabbf7cfac18b5b3c334e22a3b337a11
ayj8201 <junny.on.top@gmail.com>
2026-06-27 11:31:24 +0900

    Increase number of training epochs from 3 to 5

--- train.py
+++ train.py
@@ -143,7 +143,7 @@ tokenized_dataset = dataset.map(tokenize)
 
 training_args = TrainingArguments(
     output_dir=OUTPUT_DIR,
-    num_train_epochs=3,
+    num_train_epochs=5,
     per_device_train_batch_size=8,
     learning_rate=2e-5,
     weight_decay=0.01,
